22b710e0fa8d708def059e74f5bcb4d6a49b0fef
KalanaNiyumal <shkniyumal@gmail.com>
2025-01-24 11:24:02 +0530

    new

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
-import { FaPhone, FaEnvelope, FaLinkedin, FaGithub, FaWhatsapp, FaUserAlt } from "react-icons/fa";  // Importing icons
+import {
+  FaPhone,
+  FaEnvelope,
+  FaLinkedin,
+  FaGithub,
+  FaWhatsapp,
+} from "react-icons/fa"; // Importing icons
 import "./Contact.css";
 import BodyContent from "../Components/BodyContent/BodyContent";
 import HeaderContent from "../Components/HeaderContent/HeaderContent";
@@ -10,26 +16,25 @@ function Contact() {
     <>
       <HeaderContent />
       <BodyContent>
-        <Container fluid className="contact-container mt-5 px-4">
-          <h1 className="text-center text-primary mb-5">Contact Details</h1>
+        <Container fluid className="contact-container mt-5 px-3 px-md-4">
+          <h1 className="text-center text-primary mb-4">Contact Details</h1>
           <Row className="justify-content-center">
-            <Col xs={12} md={8} lg={6}>
+            <Col xs={12} md={10} lg={8}>
               <Card className="shadow-lg contact-card">
-                <Card.Body className="d-flex align-items-center">
-                  {/* Left section for contact details */}
-                  <div className="contact-info ms-4">
+                <Card.Body>
+                  <div className="contact-info">
                     <h5 className="text-center text-primary mb-4">Let's Connect</h5>
                     <ListGroup variant="flush">
                       <ListGroup.Item className="d-flex align-items-center">
-                        <FaPhone size={30} className="icon-style" />
+                        <FaPhone size={20} className="icon-style" />
                         <span className="contact-detail">+94 70 504 2514</span>
                       </ListGroup.Item>
                       <ListGroup.Item className="d-flex align-items-center">
-                        <FaEnvelope size={30} className="icon-style" />
+                        <FaEnvelope size={20} className="icon-style" />
                         <span className="contact-detail">shkniyumal@gmail.com</span>
                       </ListGroup.Item>
                       <ListGroup.Item className="d-flex align-items-center">
-                        <FaWhatsapp size={30} className="icon-style" />
+                        <FaWhatsapp size={20} className="icon-style" />
                         <a
                           href="https://wa.me/+123456789"
                           target="_blank"
@@ -40,7 +45,7 @@ function Contact() {
                         </a>
                       </ListGroup.Item>
                       <ListGroup.Item className="d-flex align-items-center">
-                        <FaLinkedin size={30} className="icon-style" />
+                        <FaLinkedin size={20} className="icon-style" />
                         <a
                           href="https://www.linkedin.com/in/kalana-niyumal-18122a31b/"
                           target="_blank"
@@ -51,7 +56,7 @@ function Contact() {
                         </a>
                       </ListGroup.Item>
                       <ListGroup.Item className="d-flex align-items-center">
-                        <FaGithub size={30} className="icon-style" />
+                        <FaGithub size={20} className="icon-style" />
                         <a
                           href="https://github.com/KalanaNiyumal"
                           target="_blank"
@@ -61,10 +66,8 @@ function Contact() {
                           Kalana Niyumal
                         </a>
                       </ListGroup.Item>
-                      
                     </ListGroup>
                   </div>
-
                 </Card.Body>
               </Card>
             </Col>
